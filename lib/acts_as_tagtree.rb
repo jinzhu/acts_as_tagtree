50c08e4b77bfb2b54bdf6cb7296f21e957122e5d
wosmvp <wosmvp@gmail.com>
2008-11-13 08:52:30 +0800

    init acts_as_tagtree file

--- lib/acts_as_tagtree.rb
+++ lib/acts_as_tagtree.rb
@@ -1,1 +1,26 @@
-# ActsAsTagtree
+module ActiveRecord #:nodoc:
+  module Acts #:nodoc:
+    module Tagtree #:nodoc:
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+      
+      module ClassMethods
+        def acts_as_tagtree
+          has_many :taggings,:as => :taggable,:dependent => :destroy,:include => :tag
+          has_many :tags,:through => :taggings
+          include ActiveRecord::Acts::Tagtree::InstanceMethods
+          extend ActiveRecord::Acts::Tagtree::SingletonMethods
+        end
+      end
+      
+      module SingletonMethods
+      end
+      
+      module InstanceMethods
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send(:include, ActiveRecord::Acts::Tagtree)
