13187fabd33df376b768313005816b0f818d8416
wosmvp <wosmvp@gmail.com>
2009-01-21 16:50:39 +0800

    Add some comments

--- lib/tag_list.rb
+++ lib/tag_list.rb
@@ -6,17 +6,18 @@ class TagList < Array
 
   class << self
     def format_tag(str)
+      # "linux >  >  vim;tex > xetex"   =>   [linux>vim],[tex>xetex]
       return str.to_s.split(delimiter).map(&:strip).uniq.inject([]) do |s,x|
         s += format_level(x).to_a unless x.blank?
       end
     end
 
     def format_level(string)
-      result = string.split(level_delimiter).map(&:strip).inject([]) do |s,x|
+      # "linux >  >  vim"   =>   "linux>vim"
+      string.split(level_delimiter).map(&:strip).inject([]) do |s,x|
         x.sub!(/"(.*?)"/,'\1')
-        s = ((x.blank?) ? s : (s + x.to_a))
-      end
-      return result.join(level_delimiter)
+        s = ((x.blank?) ? s : (s + x.to_a)) # s will be nil if add nothing
+      end.join(level_delimiter)
     end
   end
 end
