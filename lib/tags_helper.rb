a02027322afdbe85b4c09705aff632bec16a5849
wosmvp <wosmvp@gmail.com>
2009-01-27 22:37:34 +0800

    update taglist helper

--- lib/tags_helper.rb
+++ lib/tags_helper.rb
@@ -8,9 +8,8 @@ module TagsHelper
   #    </ul>
   #  </li>
   def taglist(tag='')
-    (tag.blank? ? Tag.roots : tag.children).inject('') do |html,x|
-      html << "<li><a href='/tags/#{x.fullname}'>#{x.name}</a>" +
-        (x.children? ? "<ul>#{taglist(x)}</ul>" : '') + "</li>"
-    end
+    (tag.blank? ? Tag.roots : tag.children).inject('<ul>') do |s,x|
+      s << "<li><a href='/tags/#{x.fullname}'>#{x.name}</a>#{x.children? ? taglist(x):''}</li>"
+    end + "</ul>"
   end
 end
