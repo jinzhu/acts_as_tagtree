39bbe6c9b49af95f556a0fd0ef97f8764c8b1fc8
wosmvp <wosmvp@gmail.com>
2009-01-28 14:57:28 +0800

    Add tag helper: tagslink

--- lib/tags_helper.rb
+++ lib/tags_helper.rb
@@ -1,5 +1,5 @@
 module TagsHelper
-  # Linux>Vim>Ruby
+  # linux>vim>ruby
   #  <li><a href='/tags/linux'>linux</a>
   #    <ul>
   #      <li><a href='/tags/linux>vim'>vim</a>
@@ -11,5 +11,16 @@ module TagsHelper
     (tag.blank? ? Tag.roots : tag.children).inject('<ul>') do |s,x|
       s << "<li><a href='/tags/#{x.fullname}'>#{x.name}</a>#{x.children? ? taglist(x):''}</li>"
     end + "</ul>"
+  end
+
+  # linux>vim>ruby
+  # <a href='/tags/linux'>linux</a>><a href='/tags/linux>vim'>vim</a>><a href='/tags/linux>vim>ruby'>ruby</a>;
+  def tagslink(tags)
+    tags.inject('') do |s,x|
+      tag = x.split('>')
+      s += (1..tag.size).inject([]) do |z,y|
+        z << "<a href='/tags/#{tag.first(y).join('>')}'>#{tag[y-1]}</a>"
+      end.join('>') + ';'
+    end
   end
 end
